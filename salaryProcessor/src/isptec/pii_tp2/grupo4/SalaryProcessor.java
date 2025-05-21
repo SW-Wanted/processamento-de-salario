d236f3aafeeff15ff474f8517a1e30ef143bdb34
Carlos Tchípia <nevescarlos930@gmail.com>
2025-05-21 15:28:49 +0100

    Initialize salaryProcessor project structure and add main application class
    - Created project configuration files for NetBeans including genfiles.properties, private.properties, project.properties, and project.xml.
    - Set up the main application class SalaryProcessor with a basic main method.
    - Configured build and source directories, including settings for annotation processing and Java compilation.
    - Established properties for Javadoc generation and distribution settings.

---
+++ salaryProcessor/src/isptec/pii_tp2/grupo4/SalaryProcessor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package isptec.pii_tp2.grupo4;
+
+/**
+ *
+ * @author hp
+ */
+public class SalaryProcessor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
