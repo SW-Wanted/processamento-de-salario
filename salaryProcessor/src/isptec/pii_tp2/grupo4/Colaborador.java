18598a031afa175a1c133cd18b0ddd858f0e99a0
Carlos Tchípia <nevescarlos930@gmail.com>
2025-05-22 16:33:52 +0100

    Add initial implementation of Colaborador and Funcao classes; update SalaryProcessor main method

---
+++ salaryProcessor/src/isptec/pii_tp2/grupo4/Colaborador.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package isptec.pii_tp2.grupo4;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author hp
+ */
+public class Colaborador {
+    public String numero;
+    public String nome;
+    public String morada;
+    public LocalDate DataNascimento;
+    public Funcao Funcao;
+    public LocalDate DataAdmissao;
+    public String email;
+}
---
+++ salaryProcessor/src/isptec/pii_tp2/grupo4/Funcao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package isptec.pii_tp2.grupo4;
+
+/**
+ *
+ * @author hp
+ */
+public class Funcao {
+    public String codigo;
+    public String nome;
+    public double salarioBase;
+    public double bonus;
+}
